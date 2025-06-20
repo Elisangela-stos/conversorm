fc3c394989ce4ca0c71a2569dd8c7c9d3dc37d31
Elisangela-stos <elisangeladosstos@gmail.com>
2025-06-20 10:30:40 -0300

    resolvendo problemas js

--- scripts.js
+++ scripts.js
@@ -1,57 +1,57 @@
-let form = document.getElementById("converterform")
+const form = document.getElementById("converterform")
 const amount = document.getElementById("amount")
 const fromCurrency = document.getElementById("fromCurrency")
 const toCurrency = document.getElementById("toCurrency")
 const convertedAmount = document.getElementById("convertedAmount")
 const loading = document.querySelector(".loading")
-const result = document.querySelector("result")
-const error = document.querySelector("error")
+const resultDiv = document.querySelector(".result")
+const errorDiv = document.querySelector(".error")
 
-const API_URL = "https://v6.exchangerate-api.com/v6/mais-recebida/ "
+const API_URL = "https://open.er-api.com/v6/latest/";
 
 
 async function converterMoney(){
     loading.style.display = "block"
-    error.style.display = "none"
-    result.style.display = "none"
+    errorDiv.style.display = "none"
+    resultDiv.style.display = "none"
 
-
+ 
     try {
-        const response = await fetch(API_URL + fromCurrency.value)
-        const data = await response.json()
+        const response = await fetch(API_URL + fromCurrency.value);
+        const data = await response.json();
 
-        const rate = data.rates(toCurrency.value)
-        const convertedValue = (amount.value * rate).toFixed(2)
+        if(data.result === "sucess"){
+        const rate = data.rates(toCurrency.value);
+        const convertedValue = (amount.value * rate).toFixed(2);
+        convertedAmount.value = convertedValue;
+        resultDiv.style.display = "block";
 
-        convertedAmount.value = convertedValue
-        result.style.display = "block"
-
-
-        result.innerHTML = `
-
-        <div style="font-size: 1.4rem;"> 
-        ${ammount.value} ${fromCurrency.value} = ${convertedAmount.value} ${toCurrency.value}
+        resultDiv.innerHTML = `
+        <div style="font-size: 1.4rem;">
+         ${amount.value} ${fromCurrency.value} = ${convertedAmount.value} ${toCurrency.value}
         </div>
+        
+        <div style="font-size: 0.9rem; opacity 0.8; margin-top:10px">
+                    Taxa: 1 ${fromCurrency.value} = ${rate} ${toCurrency.value}
+                </div>
+                `;
+        }else {
+            throw new Error("API não retornou dados válidos");
 
-        <div style="font-size: 0.9rem; opacity: 0,8rem; margin-top:10px;">
-        taxa: 1 ${fromCurrency.value} = ${rate} ${toCurrency.value}
-        </div>
+        }
+   
 
-        `
+       } catch(err) {
+        errorDiv.style.display = "block";
+        errorDiv.innerHTML = 'Falha ao converter moeda! Tente novamente';
+        console.error(err);
+    } finally {
+        loading.style.display = "none";
     }
-
-    catch(err){
-        console.error(err)
-        error.style.display = "block"
-        error.innerHTML = `Falha ao converter moeda! Tente novamente`
-
-    }
-
-    loading.style.display = "none"
-
 }
 
-form.addEventListener("submit", function(event){
-    event.preventDefault()
-    converterMoney()
-})
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    converterMoney();
+});
+
